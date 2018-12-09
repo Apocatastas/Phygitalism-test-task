b74708d25bc9cfd06bb410dec9c95c4622ac9135
Lala Filippova <32189772+Apocatastas@users.noreply.github.com>
2018-12-09 17:11:27 +0300

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+public class Controller : MonoBehaviour
+{
+    public GameObject spherePrefab;
+    public GameObject[] balls;
+    private Camera[] cameras;
+    public int ballcount;
+    private int camcount;
+    int ballsChecksum;
+
+
+
+    private void Start()
+    {
+
+        ballcount = 4;
+        camcount = ballcount;
+        balls = new GameObject[ballcount];
+        cameras = new Camera[camcount];
+        ballsChecksum = 0;
+
+        //BALLS INSTANTIATION
+
+        for (int i = 0; i < ballcount; i++)
+
+        {
+            balls[i] = Instantiate(spherePrefab);
+            if (i == 0)
+            {
+                balls[i].GetComponent<Ball>().SetRouteFilePath("Assets/Resources/ball_path.json");
+            }
+            else 
+            {
+                balls[i].GetComponent<Ball>().SetRouteFilePath("Assets/Resources/ball_path" + (i + 1) + ".json");
+            }
+            if (balls[i] != null) 
+            {
+                ballsChecksum++;
+
+}
+
+
+        }
+        Debug.Log(ballsChecksum + " balls was throwed");
+        //--
+
+    }
+
+
+
+
+
+}
---
+++ Stoner.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stoner : MonoBehaviour
+{
+
+    private int xLen;
+    private int yLen;
+    private int zLen;
+    public int minLen;
+
+    public class JsonRawData // Storing data from JSON
+    {
+        public float[] x;
+        public float[] y;
+        public float[] z;
+
+    };
+
+    public JsonRawData fileOutputClass;
+
+
+    private void Start()
+    {
+        minLen = 0;
+        xLen = 0;
+        yLen = 0;
+        zLen = 0;
+        WhosShorter(); // min length search (if json is not complete)
+                       // filePath = "Assets/Resources/ball_path.json";
+
+
+        Debug.Log("Min Len" + minLen);
+    }
+
+
+
+    public void SetJStonerOutput(string what) 
+    {
+        fileOutputClass = JsonUtility.FromJson<JsonRawData>(what);
+    }
+
+
+   
+
+    private void WhosShorter()  // Check for shortest data stream if file is not complete. We could only use complete data
+
+    {
+
+        xLen = fileOutputClass.x.Length;
+        yLen = fileOutputClass.y.Length;
+        zLen = fileOutputClass.z.Length;
+
+        if (xLen > yLen)
+        {
+            if (yLen > zLen)
+            {
+                minLen = zLen;
+            }
+            else
+            {
+                minLen = yLen;
+            }
+
+
+        }
+        else
+        {
+            if (xLen > zLen)
+            {
+                minLen = zLen;
+            }
+            else
+            {
+                minLen = xLen;
+            }
+
+        }
+    }
+
+    public Vector3 GetPointByStep(int what)
+    {  // Getting next point of route while we got current route step
+
+        Vector3 point = Vector3.zero;
+        point.x = fileOutputClass.x[what];
+        point.y = fileOutputClass.y[what];
+        point.z = fileOutputClass.z[what];
+        return point;
+    }
+}
